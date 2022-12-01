dea4b430f1a719d1b9ac1d05a5d7b06c5fa864c4
Janna Goldstein <janna@impactinstitute.com>
2022-12-02 00:24:52 +0100

    part 2

--- dec01/Program.fs
+++ dec01/Program.fs
@@ -3,36 +3,69 @@
 let input = File.ReadLines("./input.txt")
 // let input = File.ReadLines ("./test_input.txt")
 
-type State = {
-    CurrentCount : int 
-    MaximumCount : int 
-}
 
-let initialState = {
-    CurrentCount = 0 
-    MaximumCount = 0
-}
+let findMaxCount input = 
 
-[<EntryPoint>]
-let main args = 
+    let initialState = {| CurrentCount = 0; MaximumCount = 0|}
+ 
     let finalState = 
         input 
         |> Seq.fold (fun state line -> 
             match line with 
             | "" -> 
-                {
-                    CurrentCount = 0 
-                    MaximumCount = max state.CurrentCount state.MaximumCount 
-                }    
+                {| CurrentCount = 0; MaximumCount = max state.CurrentCount state.MaximumCount |}    
             | something -> 
                 match System.Int32.TryParse(something) with 
                 | true, count -> 
-                    { state with CurrentCount = state.CurrentCount + count }
+                    {| state with CurrentCount = state.CurrentCount + count |}
                 | false, _ -> 
                     printfn "Encountered invalid line %s" something
                     state 
         ) initialState
-    max finalState.CurrentCount finalState.MaximumCount        
-    |> printfn "Maximum found to be: %i"
+    
+    max finalState.CurrentCount finalState.MaximumCount
+
+
+let findTopThree input = 
+
+    let initialState = {|
+        CurrentCount = 0 
+        TopThree = [0; 0; 0]
+    |}
+
+    let updateTopThree currentTopThree newCount = 
+        newCount :: currentTopThree
+        |> List.sort 
+        |> List.tail 
+
+    let finalState = 
+        input 
+        |> Seq.fold (fun state line -> 
+            match line with 
+            | "" -> 
+                {| CurrentCount = 0; TopThree = updateTopThree state.TopThree state.CurrentCount |}
+            | something -> 
+                match System.Int32.TryParse(something) with 
+                | true, count -> 
+                    {| state with CurrentCount = state.CurrentCount + count |}
+                | false, _ -> 
+                    printfn "Encountered invalid line %s" something 
+                    state 
+        ) initialState 
+    
+    updateTopThree finalState.TopThree finalState.CurrentCount
+
+
+[<EntryPoint>]
+let main args = 
+
+    printfn "Part 1"
+    findMaxCount input 
+    |> printfn "Maximum is: %i" 
+
+    printfn "Part 2"
+    findTopThree input 
+    |> List.sum 
+    |> printfn "Sum of top three is: %i"
 
     0
